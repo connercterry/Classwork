2fca82823421777966edecae5e3396416f5d076b
Conner <connercterry22@yahoo.com>
2018-02-27 09:04:31 -0600

    I used Eldritch knowledge to make instance variables, constructor, and compareTo, Mithrandir.

--- src/feb26/Fraction.java
+++ src/feb26/Fraction.java
@@ -1,6 +1,31 @@
 package feb26;
 
-public class Fraction
+public class Fraction implements Comparable<Fraction>
 {
+    private int num;
+    private int denom;
 
+    private Fraction(int num, int denom)
+    {
+        this.num=num;
+        this.denom=denom;
+        simplify();
+    }
+
+
+    @Override
+    public int compareTo(Fraction o)
+    {
+       int difference = this.num*o.denom - 0.num*this.denom;
+       if(difference>0)return 1;
+       else if(difference<0)return -1;
+       return 0;
+    }
+
+
+
+    private void simplify()
+    {
+
+    }
 }
