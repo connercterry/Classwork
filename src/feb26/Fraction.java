e3a7d7984b0c68dabdf125fff25b2248ed1bdda2
Conner <connercterry22@yahoo.com>
2018-02-27 09:18:48 -0600

    Continued to dive into the arcane knowledge to generate a Frunner and system.out.printed the compareTo of the fractions i made in the runner.

--- src/feb26/Fraction.java
+++ src/feb26/Fraction.java
@@ -5,7 +5,7 @@ public class Fraction implements Comparable<Fraction>
     private int num;
     private int denom;
 
-    private Fraction(int num, int denom)
+    public Fraction(int num, int denom)
     {
         this.num=num;
         this.denom=denom;
